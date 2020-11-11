1a06f9f4a593771ca0dc34cc274b12ae3c48e670
kellenrowe <kellenrowe@gmail.com>
2020-11-11 15:51:04 -0800

    solved twoArrayObject

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,12 @@
-// add whatever parameters you deem necessary
-function twoArrayObject() {
+'use strict';
+
+/** accepts two arrays and returns object with keys being the 
+ * elements of the first array and values the elements of the second.
+ */
+function twoArrayObject(keysArr, valsArr) {
+    let twoArrayObject = {};
+    for (let i = 0; i < keysArr.length; i++) {
+        twoArrayObject[keysArr[i]] = (valsArr[i] || null);
+    }
+    return twoArrayObject;
 }
