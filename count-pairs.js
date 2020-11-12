efa42966e99f72f1f1c2292568f4e1f6c841992d
Lucas Pagac <lucaspagac0@gmail.com>
2020-11-11 17:43:49 -0800

    count pairs solution found

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,27 @@
-// add whatever parameters you deem necessary
-function countPairs() {
+"use strict";
+
+/**Given an array of integers, and a number, 
+ * find the number of pairs of integers in the array 
+ * whose sum is equal to the second parameter. 
+ * You can assume that there will be no duplicate values in the array. */
+
+
+function countPairs(intsArr, targetNum) {
+  intsArr.sort((a, b) => a - b);
+  let left = 0;
+  let right = intsArr.length - 1;
+  let count = 0;
+
+  while(left < right) {
+    if(intsArr[left] + intsArr[right] === targetNum) {
+      count++;
+      right--;
+      left++;
+    } else if (intsArr[left] + intsArr[right] > targetNum) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return count;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,18 +1,26 @@
 'use strict';
 
-/** accepts an array of non 0 ints and returns the same array mutated in place with positive ints at the left and negative ints at the right */
-function separatePositive(intsArr) {
-  let allNeg = intsArr.every(n => n < 0);
-  if (allNeg) return intsArr;
+/** accepts an array of non 0 ints and 
+ * returns the same array mutated in place 
+ * with positive ints at the left and negative ints at the right */
 
-  // let stopCount = 0;
-  for (let i = 0; i < intsArr.length; i++) {
-    if (intsArr[i] < 0) {
-      let valToMove = intsArr.splice(i, 1)[0];
-      intsArr.push(valToMove);
-      if (i <= intsArr.length - 2)
-      // i--;
+function separatePositive(intsArr) {
+  let left = 0;
+  let right = intsArr.length - 1;
+
+  while (left < right) {
+    if (intsArr[left] < 0 && intsArr[right] > 0) {
+      [intsArr[left], intsArr[right]] = [intsArr[right], intsArr[left]];
+      right--;
+      left++;
+    } else if (intsArr[left] < 0 && intsArr[right] < 0) {
+      right--;
+    } else if (intsArr[left] > 0 && intsArr[right] > 0) {
+      left++;
+    } else {
+      right--;
+      left++;
     }
   }
-  return intsArr
+  return intsArr;
 }
